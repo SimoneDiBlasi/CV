90e11c8733eb4044e3a00d62942c1162654d55bd
SimoneDiBlasi <simo_dibla@hotmail.it>
2022-05-24 23:58:01 +0200

    Inserimento metadati

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang ="it" >
+     <head>
+         <title>CV Simone Di Blasi</title>
+         <meta name = "author" content ="Simone Di Blasi">
+         <meta name="copyright" content="Simone Di Blasi ">
+         <meta charset="utf-8"> 
+     </head>
+
+ 
+     <body>
+
+
+    </body>
+
+
+</html>
