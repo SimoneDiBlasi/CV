cd5c2bfedafc0065e1f0a1442edccc6930f51118
SimoneDiBlasi <simo_dibla@hotmail.it>
2022-06-13 21:23:21 +0200

    Database corretto

--- sql/database.sql
+++ sql/database.sql
@@ -1,9 +1,10 @@
 CREATE SCHEMA CV;
 
 CREATE TABLE User (
+id INT auto_increment,
 name CHAR(240) NOT NULL ,
 last_name CHAR(255) NOT NULL,
 email CHAR(255) NOT NULL,
 Description CHAR(255) NOT NULL,
-PRIMARY KEY(email)
+PRIMARY KEY(id)
 );
